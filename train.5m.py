11f9d9bca14e8f6e314ab45e4ebb669256f3d02f
Daniel Parker <daniel.parker@planixs.com>
2017-04-14 16:53:16 +0100

    Add requirements and reformat file

--- train.5m.py
+++ train.5m.py
@@ -17,27 +17,26 @@ delays = []
 shorts = []
 
 for train in TRAINS:
-  url = '{}{}'.format(train, datestring) # 2016/09/22
-  page = requests.get(url)
-  tree = html.fromstring(page.content)
-  delay = tree.xpath('//td[@class="delay"]/text()')
+    url = '{}{}'.format(train, datestring)  # 2016/09/22
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    delay = tree.xpath('//td[@class="delay"]/text()')
 
-  if len(delay) == 0 or date > end:
-  	delays.append('')
-  else:
-  	delays.append('{}|href={}'.format(delay[-1], url))
-
-  try:
-    if delay[-1] == 'On time' or 'early' in delay[-1]:
-      shorts.append('+')
+    if len(delay) == 0 or date > end:
+        delays.append('')
     else:
-      shorts.append(delay[-1])
-  except:
-    shorts.append('+')
+        delays.append('{}|href={}'.format(delay[-1], url))
 
+    try:
+        if delay[-1] == 'On time' or 'early' in delay[-1]:
+            shorts.append('+')
+        else:
+            shorts.append(delay[-1])
+    except:
+        shorts.append('+')
 
 
 print '-'.join(shorts)
 print '---'
 for delay in delays:
-  print delay
+    print delay
